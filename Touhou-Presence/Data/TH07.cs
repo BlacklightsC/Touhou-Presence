74cf2cc88e14286988e178eb8494ca7867ce15a3
BlacklightsC <pshx104@gmail.com>
2018-05-15 10:35:47 +0900

    Initial TH07

---
+++ Touhou-Presence/Data/TH07.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Touhou_Presence.Data
+{
+    internal class TH07 : TouhouBase
+    {
+        internal TH07()
+        {
+            ProgramName = "th07";
+            
+        }
+    }
+}
